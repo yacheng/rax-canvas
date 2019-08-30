977b43be854b719b8d82b137b38ee70e3fa4ac36
泠苞 <liaoshuai.ls@alibaba-inc.com>
2019-08-30 12:06:02 +0800

    feat: migrate component to Typescript

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import {createElement, Component, createRef, RefAttributes, HTMLAttributes} from 'rax';
+import {isWeex} from 'universal-env';
+import { enable, WeexBridge, Image as GImage } from 'gcanvas.js';
+import findDOMNode from 'rax-find-dom-node';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'gcanvas': any;
+    }
+  }
+}
+
+export type CanvasProps = RefAttributes<HTMLCanvasElement> & HTMLAttributes<HTMLCanvasElement>;
+
+class Canvas extends Component<CanvasProps, any> {
+  private canvas: any = createRef();
+  static createImage = () => {
+    return isWeex ? new GImage() : new Image();
+  };
+ 
+  public getContext = (type = '2d') => {
+    const canvas = findDOMNode(this.canvas.current);
+    return isWeex ? enable(canvas, { bridge: WeexBridge, debug: false, disableAutoSwap: false, disableComboCommands: false }).getContext(type) : canvas.getContext(type)
+  };
+
+  public render() {
+    const {style = {}} = this.props;
+    return isWeex ? <gcanvas {...this.props} ref={this.canvas} /> : <canvas {...this.props} width={style.width} height={style.height} ref={this.canvas} />;
+  }
+}
+
+export default Canvas;
