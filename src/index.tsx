a08623de25e04d957d0ef9b6af9eb909d2474d5d
泠苞 <liaoshuai.ls@alibaba-inc.com>
2019-09-06 10:29:20 +0800

    fix: pass the style props to element

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import {createElement, Component, createRef, HTMLAttributes} from 'rax';
-import {isWeex} from 'universal-env';
+import { createElement, Component, createRef, HTMLAttributes } from 'rax';
+import { isWeex } from 'universal-env';
 import { enable, WeexBridge, Image as GImage } from 'gcanvas.js';
 import findDOMNode from 'rax-find-dom-node';
 
@@ -14,19 +14,36 @@ declare global {
 export type CanvasProps = HTMLAttributes<HTMLCanvasElement>;
 
 class Canvas extends Component<CanvasProps, any> {
-  private canvas: any = createRef();
-  static createImage = () => {
+  private canvas = createRef<any>();
+  public static createImage = () => {
     return isWeex ? new GImage() : new Image();
   };
- 
+
   public getContext = (type = '2d') => {
     const canvas = findDOMNode(this.canvas.current);
-    return isWeex ? enable(canvas, { bridge: WeexBridge, debug: false, disableAutoSwap: false, disableComboCommands: false }).getContext(type) : canvas.getContext(type)
+    return isWeex
+      ? enable(canvas, {
+        bridge: WeexBridge,
+        debug: false,
+        disableAutoSwap: false,
+        disableComboCommands: false
+      }).getContext(type)
+      : canvas.getContext(type);
   };
 
   public render() {
-    const {style = {}} = this.props;
-    return isWeex ? <gcanvas {...this.props} ref={this.canvas} /> : <canvas {...this.props} width={style.width} height={style.height} ref={this.canvas} />;
+    const { style = {}, className, ...rest } = this.props;
+    return isWeex ? (
+      <gcanvas ref={this.canvas} style={style} {...rest}/>
+    ) : (
+      <canvas
+        ref={this.canvas}
+        style={style}
+        width={style.width}
+        height={style.height}
+        {...rest}
+      />
+    );
   }
 }
 
